3fe210d60cc33fdbd33448ad1e1c382d8b066258
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-20 04:55:24 +0300

    FE//Exracting data from response

--- react-frontend/src/components/Survey.js
+++ react-frontend/src/components/Survey.js
@@ -3,8 +3,11 @@ import React from 'react'
 const Survey = ({survey}) => {
     return (
     <div className={'survey'}>
-        <h3>{survey.survey_title}</h3>
-        <p>{survey.created_at}</p>
+        <h1>{"Survey title: " + survey.survey_title}</h1>
+        <p>{"Survey created at: " + survey.created_at}</p>
+        <p>{console.log(survey.questions.length)}</p>
+        <h4>{survey.questions[0].question}</h4>
+        <input type={survey.questions[0].question_type}/>
     </div>
     );
 };    
