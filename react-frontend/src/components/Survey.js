42cbde8c8eb8e905c5f8fd7037fad28ac0790600
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-20 09:01:25 +0300

    FE//Displaying surveys id

--- react-frontend/src/components/Survey.js
+++ react-frontend/src/components/Survey.js
@@ -4,6 +4,7 @@ const Survey = ({survey}) => {
     return (
     <div className={'survey'}>
         <h1>{"Survey title: " + survey.survey_title}</h1>
+        <p>{"Survey id: " + survey.survey_id}</p>
         <p>{"Survey created at: " + survey.created_at}</p>
         {/* <p>{console.log(survey.questions.length)}</p> */}
         {/* <h4>{survey.questions[0].question}</h4> */}
