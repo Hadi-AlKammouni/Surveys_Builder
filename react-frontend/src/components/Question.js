b24b4703ed0738bda65fbed89eee92c2d1e2a3c8
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-23 16:24:49 +0300

    FE//Displaying radio type questions

--- react-frontend/src/components/Question.js
+++ react-frontend/src/components/Question.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+
 // import Question2 from './Question2';
 
 // const Question = ({detail}) => {
@@ -17,7 +18,41 @@ import React from 'react'
 const displayAccordingToType = (detail) => {
     if (detail.question_type === "text"){
        return <input type={detail.question_type} placeholder="Type your asnwer here"/>
+    }else if (detail.question_type === "radio"){
+        return displayRadio(detail)
+    }
+}
+
+//Function to check how many option there is for every question of radio type and display them
+const displayRadio = (detail) => {
+    console.log("hi")
+    if(detail.option1 === null){
+        return <p>"No options availabe"</p>
+    }
+    else if(detail.option2 === null){
+        return <div>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1}/>
+        <label for={detail.option1}>{detail.option1}</label><br></br>
+        <p>"No other options"</p>
+    </div> 
+    }
+    else if(detail.option3 === null){
+        return <div>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1}/>
+        <label for={detail.option1}>{detail.option1}</label><br></br>
+        <input type={detail.question_type} id={detail.option2} value={detail.option2}/>
+        <label for={detail.option2}>{detail.option2}</label><br></br>
+        <p>"No other options"</p>
+    </div> 
     } 
+    return <div>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1}/>
+        <label for={detail.option1}>{detail.option1}</label><br></br>
+        <input type={detail.question_type} id={detail.option2} value={detail.option2}/>
+        <label for={detail.option2}>{detail.option2}</label><br></br>
+        <input type={detail.question_type} id={detail.option3} value={detail.option3}/>
+        <label for={detail.option3}>{detail.option3}</label><br></br>
+    </div>
 }
 
 const Question = ({detail}) => {
