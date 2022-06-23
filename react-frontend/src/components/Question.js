1a25db0231ea1b565837b220b0eb53809e55ca27
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-23 16:27:23 +0300

    FE//Limiting the choosing of radio option to 1

--- react-frontend/src/components/Question.js
+++ react-frontend/src/components/Question.js
@@ -31,26 +31,26 @@ const displayRadio = (detail) => {
     }
     else if(detail.option2 === null){
         return <div>
-        <input type={detail.question_type} id={detail.option1} value={detail.option1}/>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
         <label for={detail.option1}>{detail.option1}</label><br></br>
         <p>"No other options"</p>
     </div> 
     }
     else if(detail.option3 === null){
         return <div>
-        <input type={detail.question_type} id={detail.option1} value={detail.option1}/>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
         <label for={detail.option1}>{detail.option1}</label><br></br>
-        <input type={detail.question_type} id={detail.option2} value={detail.option2}/>
+        <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio"/>
         <label for={detail.option2}>{detail.option2}</label><br></br>
         <p>"No other options"</p>
     </div> 
     } 
     return <div>
-        <input type={detail.question_type} id={detail.option1} value={detail.option1}/>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
         <label for={detail.option1}>{detail.option1}</label><br></br>
-        <input type={detail.question_type} id={detail.option2} value={detail.option2}/>
+        <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio"/>
         <label for={detail.option2}>{detail.option2}</label><br></br>
-        <input type={detail.question_type} id={detail.option3} value={detail.option3}/>
+        <input type={detail.question_type} id={detail.option3} value={detail.option3} name="radio"/>
         <label for={detail.option3}>{detail.option3}</label><br></br>
     </div>
 }
