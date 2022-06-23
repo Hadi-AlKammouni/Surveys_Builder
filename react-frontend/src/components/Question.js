5f4f7fac949bd2e93f01bd558270c05c3decdb7c
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-23 17:16:25 +0300

    FE//Displaying checkbox questions

--- react-frontend/src/components/Question.js
+++ react-frontend/src/components/Question.js
@@ -20,38 +20,42 @@ const displayAccordingToType = (detail) => {
        return <input type={detail.question_type} placeholder="Type your asnwer here"/>
     }else if (detail.question_type === "radio"){
         return displayRadio(detail)
+    }else if (detail.question_type === "checkbox"){
+        return <div>
+            <input type={detail.question_type} id={detail.question_type} value={detail.option1}/>
+            <label for={detail.question_type}>{detail.option1}</label><br/>
+        </div>
     }
+    
 }
 
-//Function to check how many option there is for every question of radio type and display them
+//Function to check how many options there are for every question of radio type and display them
 const displayRadio = (detail) => {
-    console.log("hi")
     if(detail.option1 === null){
         return <p>"No options availabe"</p>
     }
     else if(detail.option2 === null){
         return <div>
         <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
-        <label for={detail.option1}>{detail.option1}</label><br></br>
+        <label for={detail.option1}>{detail.option1}</label><br/>
         <p>"No other options"</p>
     </div> 
     }
     else if(detail.option3 === null){
         return <div>
         <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
-        <label for={detail.option1}>{detail.option1}</label><br></br>
+        <label for={detail.option1}>{detail.option1}</label><br/>
         <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio"/>
-        <label for={detail.option2}>{detail.option2}</label><br></br>
-        <p>"No other options"</p>
+        <label for={detail.option2}>{detail.option2}</label><br/>
     </div> 
     } 
     return <div>
         <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
-        <label for={detail.option1}>{detail.option1}</label><br></br>
+        <label for={detail.option1}>{detail.option1}</label><br/>
         <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio"/>
-        <label for={detail.option2}>{detail.option2}</label><br></br>
+        <label for={detail.option2}>{detail.option2}</label><br/>
         <input type={detail.question_type} id={detail.option3} value={detail.option3} name="radio"/>
-        <label for={detail.option3}>{detail.option3}</label><br></br>
+        <label for={detail.option3}>{detail.option3}</label><br/>
     </div>
 }
 
@@ -61,7 +65,7 @@ const Question = ({detail}) => {
             {detail.map((detail)=>(
                 <div>
                     <div className="form-control">
-                        <label >{"Question: " + detail.question}</label>
+                        <label ><b>{"Question: " + detail.question}</b></label>
                         <div>
                           {displayAccordingToType(detail)}
                         </div>
