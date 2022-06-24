b0c5b8d21e6c4ba57125ef319a184e0b6dcc77ea
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-24 13:43:00 +0300

    FE//Working by fill survey option

--- react-frontend/src/components/Question.js
+++ react-frontend/src/components/Question.js
@@ -17,16 +17,15 @@ import React from 'react';
 
 const displayAccordingToType = (detail) => {
     if (detail.question_type === "text"){
-       return <input type={detail.question_type} placeholder="Type your asnwer here"/>
+       return <input type={detail.question_type} placeholder="Type your asnwer here" onChange={handleChange}/>
     }else if (detail.question_type === "radio"){
         return displayRadio(detail)
     }else if (detail.question_type === "checkbox"){
         return <div>
-            <input type={detail.question_type} id={detail.question_type} value={detail.option1}/>
+            <input type={detail.question_type} id={detail.question_type} value={detail.option1} onChange={handleChange}/>
             <label for={detail.question_type}>{detail.option1}</label><br/>
         </div>
     }
-    
 }
 
 //Function to check how many options there are for every question of radio type and display them
@@ -36,42 +35,58 @@ const displayRadio = (detail) => {
     }
     else if(detail.option2 === null){
         return <div>
-        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio" onChange={handleChange}/>
         <label for={detail.option1}>{detail.option1}</label><br/>
         <p>"No other options"</p>
     </div> 
     }
     else if(detail.option3 === null){
         return <div>
-        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio" onChange={handleChange}/>
         <label for={detail.option1}>{detail.option1}</label><br/>
-        <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio"/>
+        <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio" onChange={handleChange}/>
         <label for={detail.option2}>{detail.option2}</label><br/>
     </div> 
     } 
     return <div>
-        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio"/>
+        <input type={detail.question_type} id={detail.option1} value={detail.option1} name="radio" onChange={handleChange}/>
         <label for={detail.option1}>{detail.option1}</label><br/>
-        <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio"/>
+        <input type={detail.question_type} id={detail.option2} value={detail.option2} name="radio" onChange={handleChange}/>
         <label for={detail.option2}>{detail.option2}</label><br/>
-        <input type={detail.question_type} id={detail.option3} value={detail.option3} name="radio"/>
+        <input type={detail.question_type} id={detail.option3} value={detail.option3} name="radio" onChange={handleChange}/>
         <label for={detail.option3}>{detail.option3}</label><br/>
     </div>
 }
 
-const Question = ({detail}) => {
+function handleChange(event) {
+    console.log(event.target.value);
+}  
+
+const Question = ({detail}) => {  
+
+    // Add Data to Backend on Submit
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // {detail.map((detail)=>(
+        //     console.log(detail.question_type)
+        // ))}
+    };
+
     return (
         <>
-            {detail.map((detail)=>(
-                <div>
-                    <div className="form-control">
-                        <label ><b>{"Question: " + detail.question}</b></label>
-                        <div>
-                          {displayAccordingToType(detail)}
+            <form className="add-form" onSubmit={onSubmit}>
+                {detail.map((detail)=>(
+                    <div>
+                        <div className="form-control">
+                            <label ><b>{"Question: " + detail.question}</b></label>
+                            <div>
+                            {displayAccordingToType(detail)}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+                <input type={"submit"} value="Submit Survey" className="btn btn-block" />
+            </form> 
         </>
     );
 };   
